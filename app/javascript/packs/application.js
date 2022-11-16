d0b529a1893b3d5aebacc7b73145b3c0178bcf57
EC2 Default User <ec2-user@ip-172-31-22-26.ap-northeast-1.compute.internal>
2022-11-16 07:21:29 +0000

    [Finish]アプリケーションを完成させよう２　24章

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,7 +12,9 @@ import "jquery";
 import "popper.js";
 import "bootstrap";
 import "../stylesheets/application"
-import '@fotawesome/fortawesome-free/js/all'
+import '@fortawesome/fontawesome-free/js/all'
+
+
 
 Rails.start()
 Turbolinks.start()
